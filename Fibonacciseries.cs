1d38cd34daf27f0f868076a99d654a52986c11c9
vrushali joshi <vrushalijoshi589@mail.com>
2022-05-11 17:15:22 +0530

    Calculate Fibonacci series

---
+++ Fibonacciseries.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblemAssignment
+{
+    internal class Fibonacciseries
+    {
+        public static void AFibonacciseries()
+        {
+            int firstNumber = 0;
+            int SecondNumber = 1;
+            int nextNumber;
+            int numberOfElements;
+            Console.Write("Enter the number of elements to Print : ");
+            numberOfElements = Convert.ToInt32(Console.ReadLine());
+            if (numberOfElements < 2)
+            {
+                Console.Write("Please Enter a number greater than two");
+            }
+            else
+            {
+                //First print first and second number
+                Console.Write(firstNumber + " " + SecondNumber + " ");
+                //Starts the loop from 2 because 0 and 1 are already printed
+                for (int i = 2; i < numberOfElements; i++)
+                {
+                    nextNumber = firstNumber + SecondNumber;
+                    Console.Write(nextNumber + " ");
+                    firstNumber = SecondNumber;
+                    SecondNumber = nextNumber;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LogicalProblemAssignment 
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("0.Calculate Fibonacci series");
+            Console.Write("Enter A Choice :");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 0:
+                    Fibonacciseries.AFibonacciseries();
+
+                    break;
+            }
+        }
+    }
+}
+
